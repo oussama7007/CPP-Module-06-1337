193ceb5d22db69a007b93895c28efb3bb741fcef
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-03-28 13:52:00 +0100

    Auto update 2026-03-28 13:52:00

--- ex01/ScalarConverter.h
+++ ex01/ScalarConverter.h
@@ -11,5 +11,7 @@ class ScalarConverter
         ~ScalarConverter();
         ScalarConverter &operator=(const ScalarConverter &obj);
         ScalarConverter(const ScalarConverter &obj);
+
+        static  
     
 }

91d85af58bbc12e0a0daf3e29f2516cabb8a7007
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-03-28 14:50:31 +0100

    Auto update 2026-03-28 14:50:31

---
+++ ex01/ScalarConverter.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+#include "ScalarConverter.h"
+
+
+ScalarConverter::ScalarConverter(){}
+
+ScalarConverter::~ScalarConverter(){
+}
+
+ScalarConverter & ScalarConverter::operator=(const ScalarConverter &obj)
+{
+    return *this;
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &obj)
+{ 
+}

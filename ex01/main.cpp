c51450d094b21787095471612fd1f85cc1bab27e
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-03-28 16:12:32 +0100

    Auto update 2026-03-28 16:12:32

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,13 +3,9 @@
 #include <bits/stdc++.h>
 #include "ScalarConverter.h"
 
-void    func(std::string &str)
-{
-    std::cout << str;
-}
-int main() {
-    std::string ou = "oussama";
-    func(ou);
 
-    return 0;
+
+int main()
+{
+    
 }

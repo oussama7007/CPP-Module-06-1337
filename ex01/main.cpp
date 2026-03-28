77c8c9650bb33633553c60831412f8ce1e177ff2
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-03-28 12:46:44 +0100

    Auto update 2026-03-28 12:46:44

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,3 +3,11 @@
 
 #include <iostream>
 
+int main()
+{
+    try {
+    int* p = new int[1000000000000]; // huge allocation
+    } catch (const std::bad_alloc& e) {
+    std::cout << "Allocation failed: " << e.what() << std::endl;
+    }
+}

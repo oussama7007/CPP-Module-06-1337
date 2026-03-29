d745d811cb0ca27eb8f0f80b3e6e5e810f856d7c
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-03-29 12:32:02 +0100

    Auto update 2026-03-29 12:32:02

--- ex01/ScalarConverter.cpp
+++ ex01/ScalarConverter.cpp
@@ -18,7 +18,22 @@
 
 int main() {
 
-    float   f = std::nanf("nullptr");
-    std::cout << f << std::endl;
+    std::string input1 = "nan";
+    std::string input2 = "inf";
+
+    try{
+        int x1 = std::stoi(input1);
+        std::cout << "x1: " << x1 << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << "خطأ عند تحويل '" << input1 << "': " << e.what() << std::endl;
+    }
+
+    try {
+        int x2 = std::stoi(input2); // ❌ سيعطي خطأ أيضاً
+        std::cout << "x2: " << x2 << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << "خطأ عند تحويل '" << input2 << "': " << e.what() << std::endl;
+    }
+
 
 }

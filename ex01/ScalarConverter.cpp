5eba19074cd017bec445a3d9dde8b12a3e52244e
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-03-29 15:51:45 +0100

    Auto update 2026-03-29 15:51:45

--- ex01/ScalarConverter.cpp
+++ ex01/ScalarConverter.cpp
@@ -9,8 +9,9 @@ static void     convet(const std::string &str)
 {
     // parsing to check the type of the input 
     std::string new_string = str;
-    if(new_string.back() == 'f' && new_string != "+inf" && new_string != "+inf" && new_string != "nanf")
+    if(new_string.back() == 'f' &&  new_string != "+inf" && new_string != "nanf")
         new_string.pop_back();
+
     double dob = std::stod(new_string);
     // char
     if (dob < 0 || dob > 127 || std::isnan(dob) || std::isinf(dob))
@@ -21,21 +22,16 @@ static void     convet(const std::string &str)
         std::cout << "char: '" << static_cast<char>(dob) << "'\n";
     
     // int 
-    if(std::isnan(dob) || std::isinf(dob) || dob > __INT_MAX__ || dob < __INT_MIN__ )
+    if(std::isnan(dob) || std::isinf(dob) || dob > INT_MAX || dob < INT_MIN)
+        std::cout << "int: impossible\n";
+    else 
+        std::cout << "int: " << static_cast<int>(dob) << "\n";
+    // float
+    std::cout << "float: " << static_cast<float>(dob) << "f\n";
+    std::cout << "double: " << dob << "\n";
 }
 
 
 
 
 
-// int
-if (std::isnan(dob) || std::isinf(dob) || dob > INT_MAX || dob < INT_MIN)
-    std::cout << "int: impossible\n";
-else
-    std::cout << "int: " << static_cast<int>(dob) << "\n";
-
-// float
-std::cout << "float: " << static_cast<float>(dob) << "f\n";
-
-// double
-std::cout << "double: " << dob << "\n";
--- ex01/ScalarConverter.h
+++ ex01/ScalarConverter.h
@@ -4,6 +4,7 @@
 
 #include <cmath>
 #include <iostream>
+#include  <climits>
 
 class ScalarConverter 
 {
